26289f1317069026b0ed257bf1f157cdb94f41ad
jfitzsimmons <joefitzsimmons@Corinnes-MacBook-Pro.local>
2018-09-30 17:55:00 -0500

    add and delete done / streamline / multi select

--- js/init.js
+++ js/init.js
@@ -8,52 +8,45 @@ function responsiveNav(x) {
   }
 }
 
-$('a[href^="#"]').on('click', function(event) {
-  let target = $(this.getAttribute('href'));
-  if (target.length) {
-    event.preventDefault();
-    $('html, body').stop().animate({
-      scrollTop: (target.offset().top) - ($('nav').height() + 43)
-    }, 1000);
-  }
-});
-
-
-function ListProbs() {
-    var x = document.getElementById("sbox");
-        var txt1 = document.getElementById("ProbAreaFrom").value;
-        var option = document.createElement("option");
-        option.text = txt1
-        x.add(option);
-        AddMarkup(x);
-    }
-
-function DeleteProbs() {
-    var x = document.getElementById("sbox");
-    for (var i = 0; i < x.options.length; i++) {
-        if (x.options[i].selected) {
-            x.options[i].remove();
-        }
-    }
-    RemoveMarkup(x);
-}
-
-function AddMarkup(sel) {
-  let links = $('.my-link');
-  $(sel.options).each(function( i ) {
-    if (typeof links[i] === 'undefined') {
-      $( ".my-nav" ).append( `<a class="my-link" href="#${this.text}">${this.text}</a>` );
-      $( "section" ).last().after( `<section id="${this.text}" class="flex"><h1>${this.text}</h1></section>` );
+function NavClick() {
+  $('a[href^="#"]').on('click', function(event) {
+    let target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: (target.offset().top) - ($('nav').height() + 43)
+      }, 1000);
     }
   });
 }
 
-function RemoveMarkup(sel) {
-  let links = $('.my-link');
-  $(links).each(function( i ) {
-    if (typeof sel.options[i] === 'undefined') {
-      $(`a[href="#${this.text}"]`).remove();
-      $(`#${this.text}`).remove();
-    }
+function AddSection() {
+  var x = document.getElementById("section-select");
+  var txt = document.getElementById("AddSectionInput").value;
+  var option = document.createElement("option");
+  option.text = txt
+  x.add(option);
+  $('#AddSectionInput').val('');
+  AddMarkup(txt);
+}
+
+function DeleteSections() {
+  let selectOps = $('#section-select option:selected');
+  selectOps.remove();
+  RemoveMarkup(selectOps);
+}
+
+function AddMarkup(txt) {
+  $(".my-nav").append(`<a class="my-link" href="#${txt}">${txt}</a>`);
+  $("section").last().after(`<section id="${txt}" class="flex"><h1>${txt}</h1></section>`);
+  NavClick();
+}
+
+function RemoveMarkup(selectOps) {
+  $(selectOps).each(function() {
+    $(`a[href="#${this.text}"]`).remove();
+    $(`#${this.text}`).remove();
   });
 }
+
+NavClick();
